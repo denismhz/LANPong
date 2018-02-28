a20867a539493d9ba1b067781134bbcaa100add8
Denis Manherz <denis.manherz@gmx.de>
2018-02-28 13:08:00 +0100

    Add PongBall
    -Trying to figure out the Vectors

---
+++ LANPong/Assets/PongBall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PongBall : MonoBehaviour {
+    Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //direction = collision.transform.position;
+        //rigidBody.velocity = direction + bounceVelocity;
+        rigidBody.velocity = new Vector3(1f * CrossPlatformInputManager.GetAxis("Horizontal") , 20f, 0f);
+    }
+}
