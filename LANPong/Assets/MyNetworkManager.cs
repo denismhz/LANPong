06a920b2bad5dfb07ef77279736e78d906bfe138
Denis Manherz <denis.manherz@gmx.de>
2018-02-28 12:27:04 +0100

    Add Players
    +Added 2 Startpositions for players(round robin?)
    +Added NetworkManager(and HUD)

---
+++ LANPong/Assets/MyNetworkManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+    public void MyStartHost()
+    {
+        StartHost();
+        Debug.Log("Host started");
+    }
+}
---
+++ LANPong/Assets/Player/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+    [Tooltip("Set at least to 3")]
+    public float speed = 3f;
+
+    private MyNetworkManager networkManager;
+
+	// Use this for initialization
+	void Start () {
+        networkManager = FindObjectOfType<MyNetworkManager>();
+        //transform.position = networkManager.GetStartPosition().position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        float xPos = CrossPlatformInputManager.GetAxis("Horizontal") * speed * Time.deltaTime;
+        transform.Translate(new Vector2(xPos, 0));
+        //Debug.Log("Transform pos: " + transform.position + " xPos: " + xPos);
+	}
+}
