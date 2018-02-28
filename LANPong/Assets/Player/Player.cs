346de7e94d207f6e232a06d108bfbbccbd7ed70b
Denis Manherz <denis.manherz@gmx.de>
2018-02-28 17:38:53 +0100

    New Try
    with 2dObjects

---
+++ LANPong/Assets/Player/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+    public float speed = 30;
+
+    private Rigidbody2D rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        if (!isClient)
+        {
+            return;
+        }
+        float input = CrossPlatformInputManager.GetAxis("Vertical");
+        //TEST moving with transform
+        rigidBody.velocity = new Vector2(0, input * speed);
+    }
+}
